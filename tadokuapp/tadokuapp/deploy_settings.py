1d30475db3a236ffa9a847462f88273b6129b696
rta_28 <rtasansax@gmail.com>
2021-10-23 22:28:22 +0900

    add: postgresql settings #2

--- tadokuapp/tadokuapp/deploy_settings.py
+++ tadokuapp/tadokuapp/deploy_settings.py
@@ -1,2 +1,12 @@
+import dj_database_url
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+
+db_from_env = dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config()
+}
