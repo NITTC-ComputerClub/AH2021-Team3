b681c1b0d5c0cb7ab3e273e469bf41d416d883b9
lemonadern <taishi.develop@gmail.com>
2021-10-27 22:43:52 +0900

    feat: index view and template #15

--- tadokuapp/tadoku/urls.py
+++ tadokuapp/tadoku/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from . import views
+from .views import IndexView
+
+app_name = 'tadoku'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', IndexView.as_view(), name='index'),
 ]
