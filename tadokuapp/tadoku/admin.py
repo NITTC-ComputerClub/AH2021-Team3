c2b9d24b4cdc0051f531125d025c5b5ff40e851d
rta_28 <rtasansax@gmail.com>
2021-10-28 23:36:26 +0900

    add models to admin

--- tadokuapp/tadoku/admin.py
+++ tadokuapp/tadoku/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Books, ReadBooks
 # Register your models here.
+
+
+class BooksAdmin(admin.ModelAdmin):
+    model = Books
+
+
+class ReadBooksAdmin(admin.ModelAdmin):
+    model = ReadBooks
+
+
+admin.site.register(Books, BooksAdmin)
+admin.site.register(ReadBooks, ReadBooksAdmin)
