72f49b3290aff2462631c2b54d1010f4b5c064a3
rta_28 <rtasansax@gmail.com>
2021-10-28 22:10:03 +0900

    add: models #19

--- tadokuapp/tadoku/models.py
+++ tadokuapp/tadoku/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+
+
+class Books(models.Model):
+    title = models.CharField(null=False)
+    words = models.IntegerField(null=False)
+    series = models.CharField(null=True, blank=True)
+    yl = models.SmallIntegerField(null=True, blank=True)
+    isbn = models.CharField(null=False, unique=True)
+
+
+class ReadBooks(models.Model):
+    RATE_CHOICES = [
+        (1, '★'),
+        (2, '★★'),
+        (3, '★★★'),
+        (4, '★★★★'),
+        (5, '★★★★★'),
+    ]
+
+    reader = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
+    book = models.ForeignKey(Books, on_delete=models.CASCADE, null=False)
+    comment = models.CharField(null=True, blank=True)
+    rate = models.SmallIntegerField(
+        null=True, blank=True, choices=RATE_CHOICES)
+    read_at = models.DateField(auto_now_add=True)
