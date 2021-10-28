2531181b933b53225d133b090bf16f225da3abdb
rta_28 <rtasansax@gmail.com>
2021-10-28 23:36:17 +0900

    fix: add max_length to CharField (Required)

--- tadokuapp/tadoku/models.py
+++ tadokuapp/tadoku/models.py
@@ -6,11 +6,11 @@ from django.conf import settings
 
 
 class Books(models.Model):
-    title = models.CharField(null=False)
+    title = models.CharField(null=False, max_length=100)
     words = models.IntegerField(null=False)
-    series = models.CharField(null=True, blank=True)
+    series = models.CharField(null=True, blank=True, max_length=100)
     yl = models.SmallIntegerField(null=True, blank=True)
-    isbn = models.CharField(null=False, unique=True)
+    isbn = models.CharField(null=False, unique=True, max_length=13)
 
 
 class ReadBooks(models.Model):
@@ -25,7 +25,7 @@ class ReadBooks(models.Model):
     reader = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
     book = models.ForeignKey(Books, on_delete=models.CASCADE, null=False)
-    comment = models.CharField(null=True, blank=True)
+    comment = models.CharField(null=True, blank=True, max_length=500)
     rate = models.SmallIntegerField(
         null=True, blank=True, choices=RATE_CHOICES)
     read_at = models.DateField(auto_now_add=True)
